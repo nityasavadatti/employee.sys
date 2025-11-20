b5c9712aed7c51ccfafd11d31157d51d00bf9bcc
nityasavadatti <nityasavadatti@39gmail.com>
2025-11-20 15:17:49 +0530

    save

---
+++ sys.py
@@ -0,0 +1,11 @@
+import sys
+emp_name=sys.argv[1]
+emp_id=sys.argv[2]
+emp_age=sys.argv[3]
+emp_salary=sys.argv[4]
+emp_exp=sys.argv[5]
+print("name of the employee",emp_name)
+print("id of the employee",emp_id)
+print("age of the employee",emp_age)
+print("salary of the employee",emp_salary)
+print("exp of the employee",emp_exp)
