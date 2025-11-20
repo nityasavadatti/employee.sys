d75e59ed5239201b0d34c81d580b387085228e90
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 16:05:17 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,9 +1,9 @@
 import sys 
-emp_name=sys.argv[1]
-emp_id=sys.argv[2]
-emp_salary=sys.argv[3]
-emp_exp=sys.argv[4]
-print("name of the employee",emp_name)
-print("id of the employee",emp_id)
-print("salary of the employee",emp_salary)
-print("exp of the employee",emp_exp)
+name=sys.argv[1]
+id=sys.argv[2]
+salary=sys.argv[3]
+exp=sys.argv[4]
+print("name of the employee", name)
+print("id of the employee", id)
+print("salary of the employee", salary)
+print("exp of the employee", exp)
