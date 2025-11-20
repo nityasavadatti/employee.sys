efd7dd762b5b89e4db542dc4b4e2fe9819c888a9
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 16:29:35 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,14 +1,15 @@
-
-  
- 
 import sys
 
-name=sys.argv[1]
-id=sys.argv[2]
-salary=sys.argv[3]
-exp=sys.argv[4]
+if len(sys.argv) != 5:
+    print("Usage: python sys.py <name> <id> <salary> <experience>")
+    sys.exit()
 
-print("name of the employee",name)
-print("id of the employee",id)
-print("salary of the employee",salary)
-print("exp of the employee",exp)
+name = sys.argv[1]
+id = sys.argv[2]
+salary = sys.argv[3]
+exp = sys.argv[4]
+
+print("name of the employee:", name)
+print("id of the employee:", id)
+print("salary of the employee:", salary)
+print("exp of the employee:", exp)
