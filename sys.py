1e8f2ecaa161f1a441ba9526a4c805842e179b04
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 16:19:57 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,11 +1,18 @@
-import sys 
-if len(sys.argv)!=5:
+
   
- name=sys.argv[1]
- id=sys.argv[2]
- salary=sys.argv[3]
- exp=sys.argv[4]
- print("name of the employee", name)
- print("id of the employee", id)
- print("salary of the employee", salary)
- print("exp of the employee", exp)
+ 
+import sys
+#check if correct number of arguments a
+if len(sys.argv)!5:
+  print("usage:python sys.py<name><roll>")
+  sys.exit(1)
+#sys.argv[0] is always the program name
+name=sys.argv[1]
+id=sys.argv[2]
+salary=sys.argv[3]
+exp=sys.argv[4]
+
+print("name of the employee", name)
+print("id of the employee", id)
+print("salary of the employee", salary)
+print("exp of the employee", exp)
